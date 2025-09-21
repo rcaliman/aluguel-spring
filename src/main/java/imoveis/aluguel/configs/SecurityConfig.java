f338a6f9a1c681c1505226c5077b0af4e996104e
Rodrigo Caliman <rcaliman@gmail.com>
2025-09-21 10:20:31 -0300

    remoção de warnings

--- src/main/java/imoveis/aluguel/configs/SecurityConfig.java
+++ src/main/java/imoveis/aluguel/configs/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+// import static org.springframework.security.config.Customizer.withDefaults;
 
 import imoveis.aluguel.entities.User;
 import imoveis.aluguel.repositories.UserRepository;
